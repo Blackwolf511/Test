9ec79eb0b0d69342d22ae41a32280c585b701443
Бабуров Никита Александрович <ip511s02@awp01.csc.local>
2016-05-21 12:58:41 +0600

    Fix

--- test/test.c
+++ test/test.c
@@ -1,7 +1,17 @@
 #include "ctest.h"
 #include "fquad.h"
-
 CTEST(testquad, quad){
+
+	double a = 1,b = -2,c = 1;
+	double x1, x2;
+	quad(a, b, c ,&x1, &x2);
+
+	ASSERT_DBL_NEAR(176, x1);
+	ASSERT_DBL_NEAR(1, x2);
+
+}
+
+CTEST(testquad, quad1){
 
 	double a = 1,b = -2,c = 1;
 	double x1, x2;
@@ -9,6 +19,5 @@ CTEST(testquad, quad){
 
 	ASSERT_DBL_NEAR(1, x1);
 	ASSERT_DBL_NEAR(1, x2);
+
 }
-	
-	
