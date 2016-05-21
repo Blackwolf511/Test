674491a93d0154d55ca92dbad62b3f28ee6ab9b7
Бабуров Никита Александрович <ip511s02@awp01.csc.local>
2016-05-21 13:27:18 +0600

    Fix2

--- test/test.c
+++ test/test.c
@@ -21,3 +21,13 @@ CTEST(testquad, quad1){
 	ASSERT_DBL_NEAR(1, x2);
 
 }
+
+CTEST(testquad, quad2){
+	double a = 1,b = -2,c = 1;
+	double x1, x2;
+	quad(a, b, c ,&x1, &x2);
+
+	ASSERT_DBL_NEAR(0, x1);
+	ASSERT_DBL_NEAR(2, x2);
+
+}
